bdbd1a3570139bbc6d3bbc6feee5b6a04678c11d
Jan Brønd <jcb@lite.dk>
2014-10-21 12:48:47 +0200

    Create ActigraphInfo.java

---
+++ ActigraphInfo.java
@@ -0,0 +1,179 @@
+package com.video4coach.gt3x;
+
+public class ActigraphInfo {
+
+	/*
+	Serial Number: NEO1F07120490
+	Device Type: GT3XPlus
+	Firmware: 3.2.1
+	Battery Voltage: 3.91
+	Sample Rate: 50
+	Start Date: 635185437600000000
+	Stop Date: 0
+	TimeZone: 01:00:00
+	Download Date: 635185440370000000
+	Board Revision: 5
+	Unexpected Resets: 0
+	Sex: Male
+	Height: 172
+	Mass: 70
+	Age: 45
+	Race: White / Caucasian
+	Limb: Waist
+	Side: Right
+	Dominance: Dominant
+	DateOfBirth: 620796379200000000
+	Subject Name: JANBROND
+
+	 */
+	
+	String SerialNumber;
+	String DeviceType;
+	String FirmwareVersion;
+	double BatteryVoltage;
+	int SampleRate;
+	long StartDate;
+	long StopDate;
+	String TimeZone;
+	long DownloadDate;
+	int BoardRevision;
+	int UnexpectedResets;
+	String Sex;
+	int Height;
+	int Mass;
+	int Age;
+	String Race;
+	String Limb;
+	String Side;
+	String Dominance;
+	long DateOfBirth;
+	String SubjectName;
+	
+	public String getSerialNumber() {
+		return SerialNumber;
+	}
+	public void setSerialNumber(String serialNumber) {
+		SerialNumber = serialNumber;
+	}
+	public String getDeviceType() {
+		return DeviceType;
+	}
+	public void setDeviceType(String deviceType) {
+		DeviceType = deviceType;
+	}
+	public String getFirmwareVersion() {
+		return FirmwareVersion;
+	}
+	public void setFirmwareVersion(String firmwareVersion) {
+		FirmwareVersion = firmwareVersion;
+	}
+	public double getBatteryVoltage() {
+		return BatteryVoltage;
+	}
+	public void setBatteryVoltage(double batteryVoltage) {
+		BatteryVoltage = batteryVoltage;
+	}
+	public int getSampleRate() {
+		return SampleRate;
+	}
+	public void setSampleRate(int sampleRate) {
+		SampleRate = sampleRate;
+	}
+	public long getStartDate() {
+		return StartDate;
+	}
+	public void setStartDate(long startDate) {
+		StartDate = startDate;
+	}
+	public long getStopDate() {
+		return StopDate;
+	}
+	public void setStopDate(long stopDate) {
+		StopDate = stopDate;
+	}
+	public String getTimeZone() {
+		return TimeZone;
+	}
+	public void setTimeZone(String timeZone) {
+		TimeZone = timeZone;
+	}
+	public long getDownloadDate() {
+		return DownloadDate;
+	}
+	public void setDownloadDate(long downloadDate) {
+		DownloadDate = downloadDate;
+	}
+	public int getBoardRevision() {
+		return BoardRevision;
+	}
+	public void setBoardRevision(int boardRevision) {
+		BoardRevision = boardRevision;
+	}
+	public int getUnexpectedResets() {
+		return UnexpectedResets;
+	}
+	public void setUnexpectedResets(int unexpectedResets) {
+		UnexpectedResets = unexpectedResets;
+	}
+	public String getSex() {
+		return Sex;
+	}
+	public void setSex(String sex) {
+		Sex = sex;
+	}
+	public int getHeight() {
+		return Height;
+	}
+	public void setHeight(int height) {
+		Height = height;
+	}
+	public int getMass() {
+		return Mass;
+	}
+	public void setMass(int mass) {
+		Mass = mass;
+	}
+	public int getAge() {
+		return Age;
+	}
+	public void setAge(int age) {
+		Age = age;
+	}
+	public String getRace() {
+		return Race;
+	}
+	public void setRace(String race) {
+		Race = race;
+	}
+	public String getLimb() {
+		return Limb;
+	}
+	public void setLimb(String limb) {
+		Limb = limb;
+	}
+	public String getSide() {
+		return Side;
+	}
+	public void setSide(String side) {
+		Side = side;
+	}
+	public String getDominance() {
+		return Dominance;
+	}
+	public void setDominance(String dominance) {
+		Dominance = dominance;
+	}
+	public long getDateOfBirth() {
+		return DateOfBirth;
+	}
+	public void setDateOfBirth(long dateOfBirth) {
+		DateOfBirth = dateOfBirth;
+	}
+	public String getSubjectName() {
+		return SubjectName;
+	}
+	public void setSubjectName(String subjectName) {
+		SubjectName = subjectName;
+	}
+	
+}
